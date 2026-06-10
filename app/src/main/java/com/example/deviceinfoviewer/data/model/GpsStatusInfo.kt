5c1b1c97d23bf2e3292acad44294ebddae5509f1
Final Station <15992567646@139.com>
2026-06-10 13:54:48 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/deviceinfoviewer/data/model/GpsStatusInfo.kt
@@ -0,0 +1,12 @@
+package com.example.deviceinfoviewer.data.model
+
+data class GpsStatusInfo(
+    var gpsEnabled: Boolean = false,
+    var fixAcquired: Boolean = false,
+    var latitude: Double = Double.NaN,
+    var longitude: Double = Double.NaN,
+    var accuracy: Float = Float.NaN,
+    var satelliteCount: Int = 0,
+    var fixSatelliteCount: Int = 0,
+    var satellites: MutableList<GpsSatelliteInfo> = mutableListOf()
+)
